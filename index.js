af2013918e6fe7a3267426a52a9101b640c48fb8
jagathguru <jagathguru.k@pointelsolutions.com>
2026-07-15 15:47:40 +0530

    Implemented secret manager concept.

--- index.js
+++ index.js
@@ -1,72 +1,66 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
- 
-// GCP HTTP functions use (req, res) signature
+const crypto = require("crypto");
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+
+const client = new SecretManagerServiceClient();
+
+const algorithm = "aes-256-cbc";
+
+async function getSecret() {
+    const projectId = process.env.projectId;
+    const secretName = "greenm-secret";
+
+    const [version] = await client.accessSecretVersion({
+        name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
+    });
+
+    const secretValue = version.payload.data.toString("utf8");
+
+    console.log(secretValue);
+    return JSON.parse(secretValue);
+}
+
 exports.handler = async (req, res) => {
     try {
         console.log("Entered....");
-       
-        // ===============================
-        // Validate Basic Authentication
-        // ===============================
-        // Express normalizes headers to lowercase automatically
+
         const authHeader = req.headers['authorization'];
-        console.log("authHeader " + authHeader);
-       
-        // if (!authHeader || !authHeader.startsWith("Basic ")) {
-        //     return res.status(401).json({
-        //         message: "Missing Basic Authorization header"
-        //     });
-        // }
- 
-        // const encodedCredentials = authHeader.split(" ")[1];
-        // const [username, password] = Buffer.from(encodedCredentials, "base64")
-        //     .toString("utf8")
-        //     .split(":");
+        const secret = await getSecret();
+        const secretUserName = secret.username;
+        const secretPassword = decrypt(secret.password, secret.encryptKey, secret.encryptIV);
         const username = req.headers['username'];
         const password = req.headers['password'];
-           
-        console.log("username " + username);
-        console.log("password " + password);
-       
+
         if (
-            username !== process.env.username ||
-            password !== process.env.password
+            username !== secretUserName ||
+            password !== secretPassword
         ) {
             return res.status(401).json({
                 message: "Invalid username or password"
             });
         }
- 
+
         console.log("Basic Authentication successful.");
- 
-        // ===============================
-        // Create JWT
-        // ===============================
         const tokenEndpoint = process.env.tokenEndpoint;
- 
-        // Note: Ensure the './certs/private.key' folder is included inside your zip file
+
         const privateKey = fs.readFileSync('./certs/private.key', 'utf8');
-        console.log("privateKey " + privateKey);
         const now = Math.floor(Date.now() / 1000);
- 
+
         const payload = {
             jti: uuidv4(),
             iat: now,
             exp: now + 300
         };
- 
+
         const token = jwt.sign(payload, privateKey, {
             algorithm: "RS256"
         });
- 
+
         console.log("JWT generated successfully.");
         console.log("tokenEndpoint " + tokenEndpoint);
-       
-        // ===============================
-        // Call Validation API
-        // ===============================
+
         const response = await fetch(tokenEndpoint, {
             method: "POST",
             headers: {
@@ -74,13 +68,12 @@ exports.handler = async (req, res) => {
                 "Content-Type": "application/json"
             }
         });
-       
+
         console.log("response status: " + response.status);
         const validateResult = await response.json();
- 
-        // Send the final validation result back
+
         return res.status(response.status).json(validateResult);
- 
+
     } catch (error) {
         console.error(error);
         return res.status(500).json({
@@ -88,3 +81,17 @@ exports.handler = async (req, res) => {
         });
     }
 };
+
+function encrypt(text, key, iv) {
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, "hex"), Buffer.from(iv, "hex"));
+    let encrypted = cipher.update(text, "utf8", "hex");
+    encrypted += cipher.final("hex");
+    return encrypted;
+}
+
+function decrypt(encryptedText, key, iv) {
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, "hex"), Buffer.from(iv, "hex"));
+    let decrypted = decipher.update(encryptedText, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
+}
